8db17b09edc81fdfdf3ed8fa09af0ee296165b4f
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-07-16 01:04:05 +0100

    Card hovering system

--- CoventPlannerUnity/Assets/Scripts/DialogueCard.cs
+++ CoventPlannerUnity/Assets/Scripts/DialogueCard.cs
@@ -9,6 +9,8 @@ public class DialogueCard : MonoBehaviour
     private int LocationInHand;
     private bool IsUsable = true;
 
+    private Vector3 VerticalChange = Vector3.up * 1.0f;
+
     private void Awake()
     {
         SpriteRenderer = GetComponent<SpriteRenderer>();
@@ -21,23 +23,33 @@ public class DialogueCard : MonoBehaviour
         SpriteRenderer.color = Color.white;
     }
 
-    public void SetLocation(int location)
+    public void SetHandLocation(int location)
     {
         LocationInHand = location;
+        SetZLocation(LocationInHand);
+    }
+
+    private void SetZLocation(int location)
+    {
         SpriteRenderer.sortingOrder = location;
+        Vector3 pos = transform.position;
+        pos.z = -0.01f * location;
+        transform.position = pos;
     }
 
     public void HoverCard()
     {
         // ~~~ instant size/position change
+        transform.Translate(VerticalChange);
         // bump to front of sorting ~~~ could do with better way to get number than set to 20
-        SpriteRenderer.sortingOrder = 20;
+        SetZLocation(20);
     }
 
     public void UnHoverCard()
     {
         // ~~~ shrink back down
-        SpriteRenderer.sortingOrder = LocationInHand;
+        transform.Translate(-VerticalChange);
+        SetZLocation(LocationInHand);
     }
 
     public void ShowMatchup(BattleOpponentSO opponent)
@@ -50,5 +62,15 @@ public class DialogueCard : MonoBehaviour
     {
         IsUsable = state;
         SpriteRenderer.color = Color.grey;
+    }
+
+    private void OnMouseEnter()
+    {
+        HoverCard();
+    }
+
+    private void OnMouseExit()
+    {
+        UnHoverCard();
     }
 }
