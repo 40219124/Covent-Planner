fe9b0e9c0cc30de1b838a3a13f9a92eb6fa7f25e
Daniel Korsah <danielkorsah97@gmail.com>
2021-07-18 22:48:12 +0100

    Audio god object and controls in Controll scene

---
+++ CoventPlannerUnity/Assets/Scripts/PopDown.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopDown : MonoBehaviour
+{
+    public static int ScreenHeight = 1080;
+    RectTransform rt;
+    Vector3 downPos;
+    Vector3 upPos;
+    public bool isDown;
+
+    void Awake()
+    {
+        //get the recttransform of the sliding panel 
+        rt = GetComponent<RectTransform>();
+        downPos = rt.localPosition;
+        upPos = downPos + new Vector3(0, PopDown.ScreenHeight, 0);
+        if (isDown)
+            SetDown();
+        else
+            SetUp();
+    }
+
+    public void SetDown()
+    {
+        rt.localPosition = downPos;
+        isDown = true;
+    }
+
+    public void SetUp()
+    {
+        rt.localPosition = upPos;
+        isDown = false;
+    }
+
+    public void Toggle()
+    {
+        if (isDown)
+            SetUp();
+        else
+            SetDown();
+    }
+}
